b2827b82b8cc4fb811129ada708095ae3c760d63
sergiosentry <109162568+serglom21@users.noreply.github.com>
2024-04-05 14:03:49 -0400

    refactor

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import sentry_sdk
 
 if __name__ == "__main__":
     sentry_sdk.init(
-        dsn="https://98d96ed5f65d466f85eb3094006eb8a4@o4504533099937792.ingest.us.sentry.io/4504710550847488",
+        dsn="[]",
     )
 
     bad_ideas.main()
